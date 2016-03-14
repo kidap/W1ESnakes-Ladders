d977e6d30ea91e49bedd07ef0566fc0779dc26ef
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-13 23:19:17 -0400

    Fixed ladder counter logic

--- W1ESnakes&Ladders/Board.m
+++ W1ESnakes&Ladders/Board.m
@@ -36,70 +36,14 @@
     int snakeCtr = 0;
     int ladderCtr = 0;
     if (difficulty == GameDifficultyEasy){
-      ladderCtr = specialSquare * 0.75;
-      snakeCtr = (int)specialSquare - ladderCtr;
+      snakeCtr = specialSquare * 0.25;
+      ladderCtr = (int)specialSquare - snakeCtr;
     } else if (difficulty == GameDifficultyMedium){
-      ladderCtr = specialSquare * 0.50;
-      snakeCtr = (int)specialSquare - ladderCtr;
+      snakeCtr = specialSquare * 0.50;
+      ladderCtr = (int)specialSquare - snakeCtr;
     } else if (difficulty == GameDifficultyHard){
-      ladderCtr = specialSquare * 0.25;
-      snakeCtr = (int)specialSquare - ladderCtr;
-    }
-    
-    //Add snakes
-    for (int x = 1; x <= snakeCtr; x++ ){
-      BOOL squareNotSet = YES;
-      
-      //Check is square was set to a snake
-      while (squareNotSet) {
-        //Get random number to set as a head of the snake
-        int randomSquareNumber = arc4random_uniform(boardSize);
-        
-        while (randomSquareNumber <=1 ||
-               randomSquareNumber >= _lastsSquareIndex) {
-          randomSquareNumber = arc4random_uniform(boardSize);
-        }
-        
-        
-        BoardSquare *selectedSquare = _boardSquares[randomSquareNumber];
-        
-        //Make sure the square is not yet a snake
-        if ( selectedSquare.square == SquareTypeNormal ){
-          BoardSquare *destinationSquare;
-          BOOL squareDestinationSet = YES;
-          int loopCtr = 0;
-          int randomSquareNumberDestination = 0;
-          
-          while (randomSquareNumberDestination <=0 ||
-                 randomSquareNumberDestination >= _lastsSquareIndex ||
-                 randomSquareNumberDestination == randomSquareNumber ||
-                 destinationSquare.square != SquareTypeNormal ){
-            randomSquareNumberDestination = arc4random_uniform(randomSquareNumber);
-            destinationSquare = _boardSquares[randomSquareNumberDestination];
-            
-            if (loopCtr >= boardSize){
-              squareDestinationSet = NO;
-              NSLog(@"Stuck on infinite loop. Trying to get out.");
-              break;
-            }
-            
-            loopCtr ++;
-          }
-          
-          if (squareDestinationSet) {
-            //Set properties of the square
-            selectedSquare.linkedTo = randomSquareNumberDestination;
-            selectedSquare.square = SquareTypeSnake;
-            NSLog(@"Snake at %d going to %d", randomSquareNumber, randomSquareNumberDestination);
-            
-            //Set the destination as a snake
-            destinationSquare.square = SquareTypeSnake;
-            squareNotSet = NO;
-          } else {
-            squareNotSet = YES;
-          }
-        }
-      }
+      snakeCtr = specialSquare * 0.75;
+      ladderCtr = (int)specialSquare - snakeCtr;
     }
     
     //Add ladders
@@ -158,7 +102,64 @@
           }
         }
       }
+      
     }
+    //Add snakes
+    for (int x = 1; x <= snakeCtr; x++ ){
+      BOOL squareNotSet = YES;
+      
+      //Check is square was set to a snake
+      while (squareNotSet) {
+        //Get random number to set as a head of the snake
+        int randomSquareNumber = arc4random_uniform(boardSize);
+        
+        while (randomSquareNumber <=1 ||
+               randomSquareNumber >= _lastsSquareIndex) {
+          randomSquareNumber = arc4random_uniform(boardSize);
+        }
+        
+        
+        BoardSquare *selectedSquare = _boardSquares[randomSquareNumber];
+        
+        //Make sure the square is not yet a snake
+        if ( selectedSquare.square == SquareTypeNormal ){
+          BoardSquare *destinationSquare;
+          BOOL squareDestinationSet = YES;
+          int loopCtr = 0;
+          int randomSquareNumberDestination = 0;
+          
+          while (randomSquareNumberDestination <=0 ||
+                 randomSquareNumberDestination >= _lastsSquareIndex ||
+                 randomSquareNumberDestination == randomSquareNumber ||
+                 destinationSquare.square != SquareTypeNormal ){
+            randomSquareNumberDestination = arc4random_uniform(randomSquareNumber);
+            destinationSquare = _boardSquares[randomSquareNumberDestination];
+            
+            if (loopCtr >= boardSize){
+              squareDestinationSet = NO;
+              NSLog(@"Stuck on infinite loop. Trying to get out.");
+              break;
+            }
+            
+            loopCtr ++;
+          }
+          
+          if (squareDestinationSet) {
+            //Set properties of the square
+            selectedSquare.linkedTo = randomSquareNumberDestination;
+            selectedSquare.square = SquareTypeSnake;
+            NSLog(@"Snake at %d going to %d", randomSquareNumber, randomSquareNumberDestination);
+            
+            //Set the destination as a snake
+            destinationSquare.square = SquareTypeSnake;
+            squareNotSet = NO;
+          } else {
+            squareNotSet = YES;
+          }
+        }
+      }
+    }
+    
     
   }
   
